e3803298cd6d5b0ad9a53116e34e6a09130251dc
Oleh Berezin <ziccer1980@gmail.com>
2022-11-08 23:08:25 +0200

    second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { PersonComponent } from './person/person.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule,
